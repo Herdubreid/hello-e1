b947c277e09117e9d408204aefb609bab410a3c3
Finnur Bragason <fbragason@outlook.com>
2017-07-18 20:48:17 +0800

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,8 @@ import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { E1ServiceModule, serverAction, initialServerState } from 'e1-service';
 import { IonicStorageModule } from '@ionic/storage';
-<<<<<<< HEAD
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-=======
->>>>>>> 35a3e59f41b5d498a8fa91a261d56cfe0ee3cb54
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 
 import { DataService } from '../data/service';
 import { E1HelperService } from '../e1/e1-helper';
@@ -17,9 +14,6 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { AbWordSearchPage } from '../pages/ab-word-search/ab-word-search';
 import { AbRevisionPage } from '../pages/ab-revision/ab-revision';
-
-import {StatusBar} from '@ionic-native/status-bar';
-import {SplashScreen} from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
@@ -31,11 +25,7 @@ import {SplashScreen} from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-<<<<<<< HEAD
-    IonicStorageModule.forRoot({ name: '__cworkorders' }),
-=======
     IonicStorageModule.forRoot({ name: '__chelloe1' }),
->>>>>>> 35a3e59f41b5d498a8fa91a261d56cfe0ee3cb54
     StoreModule.provideStore({ server: serverAction }, { server: initialServerState }),
     StoreDevtoolsModule.instrumentOnlyWithExtension(),
     E1ServiceModule
@@ -51,13 +41,8 @@ import {SplashScreen} from '@ionic-native/splash-screen';
     StatusBar,
     SplashScreen,
     { provide: ErrorHandler, useClass: IonicErrorHandler },
-<<<<<<< HEAD
-    Storage,
-    DataService,
-=======
-    StorageService,
->>>>>>> 35a3e59f41b5d498a8fa91a261d56cfe0ee3cb54
-    E1HelperService
+    E1HelperService,
+    DataService
   ]
 })
 export class AppModule { }
